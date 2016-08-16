e8c2413873a2d1350c77b3ca8a736f133d0d1b5b
alex-carlson <alex@acwd.me>
2016-08-16 11:45:34 -0500

    merged iterate and replicate together, added mode picker

--- jquery.generativeengines.js
+++ jquery.generativeengines.js
@@ -1,71 +1,63 @@
+
 $.fn.replicate = function(options) {
-  var S = $.extend({
-            // These are the defaults.
-            total: 100,
-            random: true
-        }, options );
-
-  for(var i=0;i<S.total;i++){
-
-    $('body').append($(this).clone())
-
-    if(S.random){
-      var rw = Math.random() * (window.innerWidth - $(this).width());
-      var rh = Math.random() * (window.innerHeight - $(this).height());;
-
-      $(this).css({
-        'position':'absolute',
-        'top': rh,
-        'left':rw
-      })
-
-    }
-  }; //close for loop
-
-
-};
-
-
-
-$.fn.iterate = function(options) {
-
-
 
   var S = $.extend({
             // These are the defaults.
             speed:500,
             total: 100,
-            random: true
+            random: true,
+            mode: 'once' // ONCE: Generates all elements at once.  STEP: Generates one per "speed" interval (ms)
         }, options );
 
-    var me = $(this) //store instance of this for later use in the set interval function below.
+    var me = $(this); //store instance of this for later use in the set interval function below.
     var counter = 0;
 
-    $('body').find(me).remove();
+    if(S.mode === "step"){
 
-    var gogogadget = setInterval(function(){
+      $('body').find(me).remove();
 
-      if( S.total != 0 ){ counter++ };
+      var gogogadget = setInterval(function(){
 
-      if(counter >= S.total && S.total != 0){
-        clearInterval(gogogadget);
-      }
+        if( S.total != 0 ){ counter++ };
 
-      if(S.random){
-        var rw = Math.random() * (window.innerWidth - me.width());
-        var rh = Math.random() * (window.innerHeight - me.height());
-        // console.log(rw + " || " + rh);
+        if(counter >= S.total && S.total != 0){
+          clearInterval(gogogadget);
+        }
 
-        $(me).css({
-          'position':'absolute',
-          'top': rh,
-          'left':rw
-        })
+        if(S.random){
+          var rw = Math.random() * (window.innerWidth - me.width());
+          var rh = Math.random() * (window.innerHeight - me.height());
+          // console.log(rw + " || " + rh);
 
-      }
+          $(me).css({
+            'position':'absolute',
+            'top': rh,
+            'left':rw
+          })
 
-      $('body').append(me.clone());
+        }
 
-    }, S.speed);
+        $('body').append(me.clone());
 
+      }, S.speed);
+
+    } else {
+
+      for(var i=0;i<S.total;i++){
+
+        $('body').append($(this).clone())
+
+        if(S.random){
+          var rw = Math.random() * (window.innerWidth - $(this).width());
+          var rh = Math.random() * (window.innerHeight - $(this).height());;
+
+          $(this).css({
+            'position':'absolute',
+            'top': rh,
+            'left':rw
+          })
+
+        }
+      }; //close for loop
+    }
 };
